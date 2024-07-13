76f505b4a9a7bb0455f80be978803576bd259c59
reacton <aliyasin.emir16@gmail.com>
2024-07-13 20:46:13 +0300

    actually  linking the database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,31 @@
 import Image from "next/image";
+import { desc } from "node_modules/drizzle-orm/expressions.cjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/182dfc24-86b6-4a47-b19a-9bb27cdb950f-mm3pnd.jpeg",
-  "https://utfs.io/f/7fde19a4-b3d3-48a0-a4b1-17b0ad3e8e33-xdujla.jpeg",
-  "https://utfs.io/f/de4b08a6-1aaf-4441-8fc5-683c1ce4313f-h0u3f.jpeg",
-  "https://utfs.io/f/c8612333-4190-48f1-8617-9a044fa944aa-9jd1ol.jpeg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="m-12">
-      <div className="flex flex-wrap gap-4">
-        {posts.map((post) => {
-          return (
-            <div key={post.id}>
-              <h1>{post.name}</h1>
+      <div className="mb-12 flex flex-wrap gap-4">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div className="flex flex-col gap-4" key={image.id + "-" + index}>
+            <div className="relative flex h-48 w-48">
+              <Image
+                src={image.url}
+                fill
+                sizes="100%"
+                alt="Image"
+                className="rounded-lg object-cover"
+              />
             </div>
-          );
-        })}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="relative flex h-48 w-48">
-            <Image
-              src={image.url}
-              fill
-              sizes="100%"
-              alt="Image"
-              className="rounded-lg object-cover"
-            />
+            <div>
+              <h1>{image.name}</h1>
+            </div>
           </div>
         ))}
       </div>
