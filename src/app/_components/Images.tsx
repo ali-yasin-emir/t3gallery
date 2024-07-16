d4a8335206150f9c0fa4a3978011212bc3c866ec
reacton <aliyasin.emir16@gmail.com>
2024-07-16 04:19:58 +0300

    Break queries into seperate file

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
-import { db } from "~/server/db";
+
+import { getMyImages } from "~/server/queries";
 
 const Images = async () => {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="gap-400 flex flex-wrap justify-center gap-12 p-4">
       {images.map((image) => (
         <div
-          className="flex w-[200px] h-[320px] flex-col rounded-xl bg-purple-500"
+          className="flex h-[320px] w-[200px] flex-col rounded-xl bg-purple-500"
           key={image.id}
         >
           <div className="relative h-4/6">
@@ -21,8 +20,8 @@ const Images = async () => {
               className="rounded-t-lg object-cover"
             />
           </div>
-          <div className="flex items-center h-2/6 p-4">
-            <h1 className="text-white text-sm font-thin">{image.name}</h1>
+          <div className="flex h-2/6 items-center p-4">
+            <h1 className="text-sm font-thin text-white">{image.name}</h1>
           </div>
         </div>
       ))}
